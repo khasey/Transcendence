fb59e4bd052c3e45b122dddfc4aedf3864a01c76
Khasey_Nine_Z <92310470+khasey@users.noreply.github.com>
2023-06-13 01:10:42 +0200

    Add files via upload

--- transcendence_backend/src/main.ts
+++ transcendence_backend/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaClient } from '@prisma/client'
-
 const prisma = new PrismaClient()
+// use `prisma` in your application to read and write data in your DB
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
