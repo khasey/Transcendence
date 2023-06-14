54455a0d1b150776c8619545d1c9f911a8c14f39
Khasey_Nine_Z <92310470+khasey@users.noreply.github.com>
2023-06-14 02:20:40 +0200

    Delete transcendence_frontend/src/component/backdrop directory

--- transcendence_frontend/src/component/backdrop/AddUser.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function AddUser() {
-  return (
-    <div style={{color:'white'}}>AddUser</div>
-  )
-}
-
-export default AddUser
--- transcendence_frontend/src/component/backdrop/CreateChannel.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function CreateChannel() {
-  return (
-    <div style={{color:'white'}}>CreateChannel</div>
-  )
-}
-
-export default CreateChannel
