e327b46549d504f83741ecda85ed4ef62b137fb1
Khasey_Nine_Z <92310470+khasey@users.noreply.github.com>
2023-06-21 15:56:53 +0200

    Add files via upload

--- transcendence_frontend/src/app/login/page.tsx
+++ transcendence_frontend/src/app/login/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './login.module.css'
 import { Avatar, Box, Button, Stack, TextField, styled } from '@mui/material';
 import Link from 'next/link';
@@ -9,10 +9,34 @@ import { loadFull } from "tsparticles";
 import particlesOptions from "../particles.json";
 import { ISourceOptions } from "tsparticles-engine";
 import { useCallback } from 'react';
+import axios from 'axios';
+import { User }  from '../../../../transcendence_backend/src/user/user.entity'; // Assurez-vous que le chemin est correct
+
 // import { AccountCircle } from '@mui/icons-material';
 
 
 const Profil: React.FC = () => {
+
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get<User>('http://localhost:4000/user/104440');
+        setUser(response.data);
+        console.log("rep data = " + response.data);
+      } catch (error) {
+        console.error("Erreur lors de la récupération de l'utilisateur :", error);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  // if (!user) {
+  //   return <p>Loading...</p>;
+  // }
+
   const particlesInit = useCallback(async (engine: Engine) => {
     await loadFull(engine);
 }, []);
@@ -23,7 +47,7 @@ const Profil: React.FC = () => {
           gap:'0%',
         }}>
         <Box sx={{ display: 'flex', flexDirection:'column', alignItems:'center'}}>
-          <Avatar alt="Kevin" src="" 
+          <Avatar alt="Kevin" src={user?.imageUrl} 
           sx={{
             "@media screen and (width < 1500px)":{
               width:'80px',
@@ -34,11 +58,12 @@ const Profil: React.FC = () => {
             height:'90px',
           
           }}/>
-          <div className="profil_in_name">
-            <p className="profil_in_name_text" style={{fontSize:'20px', marginTop:'15%'}}>
-              kthierry
-            </p>
-          </div>
+          
+         <div className="profil_in_name">
+                <p className="profil_in_name_text" style={{ fontSize: '20px', marginTop: '15%' }}>
+                 {user?.username}
+                </p>
+            </div>
         </Box>
         <Box>
           <TextField label="username"
