8d788762ae07aa688e0db684aa63d1af58e6c93a
Khasey_Nine_Z <92310470+khasey@users.noreply.github.com>
2023-06-23 22:07:48 +0200

    Delete transcendence_backend directory

--- transcendence_backend/dist/auth/auth.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { AuthService } from './auth.service';
-import { Response } from 'express';
-export declare class AuthController {
-    private readonly authService;
-    constructor(authService: AuthService);
-    fortyTwoAuth(): void;
-    fortyTwoAuthRedirect(req: any, res: Response): Promise<void>;
-}
