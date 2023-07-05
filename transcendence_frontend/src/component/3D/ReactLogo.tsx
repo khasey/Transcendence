ec333e2f592811956a5885f29369fbec157ad9eb
Khasey_Nine_Z <92310470+khasey@users.noreply.github.com>
2023-07-05 16:56:31 +0200

    Add files via upload

--- transcendence_frontend/src/component/3D/ReactLogo.tsx
+++ transcendence_frontend/src/component/3D/ReactLogo.tsx
@@ -23,11 +23,11 @@ export default function ReactLogo() {
       gl={{ antialias: true, pixelRatio: window.devicePixelRatio * 2 }}
       >
       <color attach="background" args={['black']} />
-      <Float speed={4} rotationIntensity={1} floatIntensity={2}>
+      <Float speed={4} rotationIntensity={3} floatIntensity={1}>
         <Atom />
       </Float>
       <EffectComposer>
-        <Bloom mipmapBlur luminanceThreshold={1} radius={0.7} />
+        <Bloom mipmapBlur luminanceThreshold={1}  />
       </EffectComposer>
     </Canvas>
     </div>
@@ -38,14 +38,14 @@ function Atom(props: AtomProps) {
   const points = useMemo(() => new THREE.EllipseCurve(0, 0, 3, 1.15, 0, 2 * Math.PI, false, 0).getPoints(100), []);
   return (
     <group {...props}>
-      <Line worldUnits points={points} color="turquoise" lineWidth={0.3} />
-      <Line worldUnits points={points} color="turquoise" lineWidth={0.3} rotation={[0, 0, 1]} />
-      <Line worldUnits points={points} color="turquoise" lineWidth={0.3} rotation={[0, 0, -1]} />
+      <Line worldUnits points={points} color="#0099ff" lineWidth={0.3} />
+      <Line worldUnits points={points} color="#0099ff" lineWidth={0.3} rotation={[0, 0, 1]} />
+      <Line worldUnits points={points} color="#0099ff" lineWidth={0.3} rotation={[0, 0, -1]} />
       <Electron position={[0, 0, 0.5]} speed={6} />
       <Electron position={[0, 0, 0.5]} rotation={[0, 0, Math.PI / 3]} speed={6.5} />
       <Electron position={[0, 0, 0.5]} rotation={[0, 0, -Math.PI / 3]} speed={7} />
       <Sphere args={[0.55, 64, 64]}>
-        <meshBasicMaterial color={[6, 0.5, 2]} toneMapped={false} />
+        <meshBasicMaterial color={[1, 10, 30]} toneMapped={false} />
       </Sphere>
     </group>
   );
@@ -62,7 +62,7 @@ function Electron({ radius = 2.75, speed = 6, ...props }: ElectronProps) {
       <Trail local width={5} length={6} color={new THREE.Color(2, 1, 10)} attenuation={(t) => t * t}>
         <mesh ref={ref}>
           <sphereGeometry args={[0.25]} />
-          <meshBasicMaterial color={[10, 1, 10]} toneMapped={false} />
+          <meshBasicMaterial color={[1, 15, 30]} toneMapped={false} />
         </mesh>
       </Trail>
     </group>
