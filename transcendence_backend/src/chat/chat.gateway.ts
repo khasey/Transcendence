fa30ea63eaa0dddd5d0586ac98b42765d64673d4
Khasey_Nine_Z <92310470+khasey@users.noreply.github.com>
2023-07-11 17:35:55 +0200

    Update chat.gateway.ts

--- transcendence_backend/src/chat/chat.gateway.ts
+++ transcendence_backend/src/chat/chat.gateway.ts
@@ -1,17 +1,15 @@
 import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'socket.io';
+import { User } from 'src/user/user.entity';
 
 @WebSocketGateway({ namespace: 'chat' })
 export class ChatGateway {
-
   @WebSocketServer()
   server: Server;
 
   @SubscribeMessage('chat message')
-  handleMessage(@MessageBody() message: { text: string, user: string }): void {
-  const { text, user } = message;
-  console.log("yo mess => " + text + " from =>" + user);
-  console.log("message recu serveur => " + text);
-  this.server.emit('chat message', text);
+  handleMessage(@MessageBody() message: { text: string; user: User }): void {
+    this.server.emit('chat message', message);
+    console.log('user => '+User)
   }
 }
