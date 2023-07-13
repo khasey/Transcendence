8458df89a7e51fcc171b7df548967e27df0cb3ff
Khasey_Nine_Z <92310470+khasey@users.noreply.github.com>
2023-07-13 18:12:24 +0200

    Delete transcendence_backend directory

--- transcendence_backend/dist/auth/auth.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { AuthService } from './auth.service';
-import { Response } from 'express';
-export declare class AuthController {
-    private readonly authService;
-    constructor(authService: AuthService);
-    fortyTwoAuth(): void;
-    fortyTwoAuthRedirect(req: any, res: Response): Promise<void>;
-    getUser(req: any, res: Response): Promise<void>;
-}
--- transcendence_backend/dist/chat/chat.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from 'typeorm';
-import { ChatMessage } from './chat.entity';
-export declare class ChatService {
-    private chatMessageRepository;
-    constructor(chatMessageRepository: Repository<ChatMessage>);
-    getAllMessages(): Promise<ChatMessage[]>;
-    createMessage(text: string, senderId: number, channelId: number): Promise<ChatMessage>;
-}
