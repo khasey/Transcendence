286198076fe5c2cb550193f1d8debba63ca9af53
Khasey_Nine_Z <92310470+khasey@users.noreply.github.com>
2023-08-07 14:39:39 +0200

    Delete transcendence_frontend directory

--- transcendence_frontend/src/component/backdrop/SelectedTabContext.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SelectedTabContext = React.createContext<{ selectedTab: {id: number, label: string}, setSelectedTab: React.Dispatch<React.SetStateAction<{id: number, label: string}>> }>({ selectedTab: {id: 1, label: 'General'}, setSelectedTab: () => { throw new Error("Used SelectedTabContext before SelectedTabProvider was rendered"); } });
-
-export default SelectedTabContext;
-
-
